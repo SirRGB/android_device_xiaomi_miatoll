52de1a2da7f92211c4b0dec04e3f866c279f1ed5
SirRGB <sirrgb@proton.me>
2026-01-20 20:06:28 +0100

    miatoll: switch to akumas defconf

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -20,7 +20,7 @@ TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):init_xiaomi_miatoll
 TARGET_RECOVERY_DEVICE_MODULES := init_xiaomi_miatoll
 
 # Kernel
-TARGET_KERNEL_CONFIG := vendor/xiaomi/miatoll_defconfig
+TARGET_KERNEL_CONFIG := midnight_defconfig
 
 # OTA assert
 TARGET_OTA_ASSERT_DEVICE := curtana,excalibur,gram,joyeuse,miatoll
